b21c3b78bbf7143b80bb5011dc7ab231fe70c2b8
onyekus001 <onyekus001@yahoo.com>
2022-10-03 16:56:50 -0700

    A function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,73 +1,72 @@
-#include <stdlib.h>
+#include "main.h"
 /**
-* strtow - char
-* @str: pointer to string params
-* Return: char
+* wrdcnt - counts the number of words in a string
+* @s: string to count
+*
+* Return: int of number of words
+*/
+int wrdcnt(char *s)
+{
+int i, n = 0;
+for (i = 0; s[i]; i++)
+{
+if (s[i] == ' ')
+{
+if (s[i + 1] != ' ' && s[i + 1] != '\0')
+n++;
+}
+else if (i == 0)
+n++;
+}
+n++;
+return (n);
+}
+/**
+* strtow -  splits a string into words
+*
+* @str: string to splear
+*
+* Return:  pointer to an array of strings (words)
 */
 char **strtow(char *str)
 {
-int i = 0, j = 0, k = 0;
-int len = 0, count = 0;
-char **f, *col;
-if (!str || !*str)
+int i, j, k, l, n = 0, qa = 0;
+char **q;
+if (str == NULL || *str == '\0')
+return (NULL);
+n = wrdcnt(str);
+if (n == 1)
+return (NULL);
+q = (char **)malloc(n * sizeof(char *));
+if (q == NULL)
+return (NULL);
+q[n - 1] = NULL;
+i = 0;
+while (str[i])
 {
+if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+{
+for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+;
+j++;
+q[qa] = (char *)malloc(j * sizeof(char));
+j--;
+if (q[qa] == NULL)
+{
+for (k = 0; k < qa; k++)
+free(q[k]);
+free(q[n - 1]);
+free(q);
 return (NULL);
 }
-while (*(str + i))
-{
-if (*(str + i) != ' ')
-{
-if (*(str + i + 1) == ' ' || *(str + i + 1) == 0)
-{
-count += 1;
+for (l = 0; l < j; l++)
+q[qa][l] = str[i + l];
+q[qa][l] = '\0';
+qa++;
+i += j;
 }
-}
+else
 i++;
 }
-if (count == 0)
-{
-return (NULL);
-}
-count += 1;
-f = malloc(sizeof(char *) * count);
-if (!f)
-{
-return (NULL);
-}
-i = 0;
-while (*str)
-{
-while (*str == ' ' && *str)
-{
-str++;
-}
-len = 0;
-while (*(str + len) != ' ' && *(str + len))
-{
-len += 1;
-}
-len += 1;
-col = malloc(sizeof(char) * len);
-if (!col)
-{
-for (k = j - 1; k >= 0; k--)
-{
-free(f[k]);
-}
-free(f);
-return (NULL);
-}
-for (k = 0; k < (len - 1);  k++)
-{
-*(col + k) = *(str++);
-}
-*(col + k) = '\0';
-*(f + j) = col;
-if (j < (count - 1))
-{
-j++;
-}
-}
-*(f + j) = NULL;
-return (f);
+return (q);
 }
